753863cf10a9d8cb57dcd7f3596a486e1631a959
Tianhua Zhao <tianhz@microsoft.com>
2017-07-20 16:59:22 +0800

    test

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Tts.ServiceProvider;
+
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        static void Test()
+        {
+            ServiceProvider serviceProvider = null;
+            using (serviceProvider = new ServiceProvider(Language.ZhCN,
+                @"C:\Users\tianhz\Documents\Visual Studio 2015\Projects\Hackthon\Hackthon\bin\Font\LinZhiling\2052", @"E:\offline\TemplateTuningTool\offlinetool\LocaleHandler"))
+            {
+                serviceProvider.SpeechSynthesizer.SetOutputToWaveStream(null);
+                serviceProvider.SpeechSynthesizer.Speak("123");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("fff");
+            Test();
         }
     }
 }
